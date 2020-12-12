151f7d9f3924cc975a46935d3c39e232032e248d
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2020-12-13 00:17:51 +0100

    types: added more under score aliases.

--- types.hpp
+++ types.hpp
@@ -13,5 +13,15 @@ using u64 = std::uint64_t;
 using u8 = std::uint8_t;
 
 class QString;
+class QStringList;
+class QFile;
+class QFileInfo;
+class QDirIterator;
+class QWidget;
 
 using string = QString;
+using string_list = QStringList;
+using file = QFile;
+using file_info = QFileInfo;
+using dir_iterator = QDirIterator;
+using widget = QWidget;
